20e357d8a4efe26c428ac03ded7429696a7f3583
Bastien-Viatge <bastien.viatge@insa-lyon.fr>
2014-11-24 11:59:07 +0100

    Début d'implémentation

---
+++ src/modele/Intersection.java
@@ -0,0 +1,17 @@
+package modele;
+
+public class Intersection {
+	
+	private int id,x,y;
+	
+	public Intersection(int id, int x, int y ){
+		this.id = id;
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getIdIntersction(){
+		return id;
+	}
+
+}
