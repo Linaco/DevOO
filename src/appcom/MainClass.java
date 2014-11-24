105ed7ca157533b8e1b27d250dfaa07b9750d7a2
2081 <robsnicolet@hotmail.fr>
2014-11-24 13:57:10 +0100

    maj
    Mise à jour du main pour aller avec le ServeurBuilder

--- src/appcom/MainClass.java
+++ src/appcom/MainClass.java
@@ -11,53 +11,15 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
+import controleur.Controleur;
+
 public class MainClass {
 
 	public static void main(String[] args) {
+		Controleur c = new Controleur();
 		
-		ServeurLivraison serveur = new ServeurLivraison();
-		int port = serveur.Demarrer();
-		
-		ServiceControleur serviceChargerPlan = new ServiceControleur(null,"charger-plan",serveur){
-			protected String getReponse(InputStream in){
-				
-				DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-			    //factory.setNamespaceAware(true);
-			    DocumentBuilder builder;
-				try {
-					builder = factory.newDocumentBuilder();
-					try {
-						Document doc = builder.parse(in);
-						// appel au service
-						return doc.toString();
-					} catch (SAXException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				} catch (ParserConfigurationException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-				/*String planXml = "";
-				String buf;
-				try{
-					while( (buf = in.readLine()) != null){
-						planXml += buf;
-					}
-				} catch (IOException e){
-					e.printStackTrace();
-				}
-				return planXml;*/
-				return null;
-			}
-		};
-		
-	    
-		
+		ServeurBuilder.nouveauServeur()
+			.deployerServicesControleur(c);
 		
 	}
 
