60f6be8ffc211189d54756d79721e509d41bf92b
Robin Nicolet <robsnicolet@hotmail.fr>
2014-11-26 08:51:50 +0100

    Ajout

---
+++ src/appcom/ServiceModele.java
@@ -0,0 +1,18 @@
+package appcom;
+
+public class ServiceModele extends ServiceHandler {
+
+	// Attributs
+	
+	
+	
+	// Constructeur
+	public ServiceModele(String nomService, ServeurLivraison serveur) {
+		super(nomService, serveur);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	// Methodes
+
+}
