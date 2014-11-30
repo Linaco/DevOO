dd526e843a1853acf69e58c7874317cbd05c3a5b
Bastien-Viatge <bastien.viatge@insa-lyon.fr>
2014-11-30 19:56:47 +0100

    Parseur xml pour plan ok et gere les erreurs

--- src/modele/Intersection.java
+++ src/modele/Intersection.java
@@ -1,7 +1,7 @@
 package modele;
 
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Intersection {
 	
@@ -9,30 +9,50 @@ public class Intersection {
 	private int id,x,y;
 	private List<Route> troncSortants;
 	
+	/**
+	 * Constructeur d'intersection, initialisation de l'ArrayList
+	 * @param id
+	 * @param x
+	 * @param y
+	 */
 	public Intersection(int id, int x, int y ){
 		this.id = id;
 		this.x = x;
 		this.y = y;
 		this.troncSortants = new ArrayList<Route>();
-
 	}
 	
-
-	public int getId(){
-		return id;
-	}
+	//Getters
+	/**
+	 * Getter de la liste de route sortant de l'intersection
+	 * @return List<Route> troncSortants
+	 */
+	public List<Route> getTroncsSortants(){return this.troncSortants;}
 	
-	public int getX(){
-		return x;
-	}
-
-
-	public void ajouterRoute(Route route) {
-		troncSortants.add(route);
-	}
-
-	public int getY(){
-		return y;
-	}
+	/**
+	 * Getter de l'id
+	 * @return id
+	 */
+	public int getId(){return id;}
 	
+	/**
+	 * getter de la position x
+	 * @return x
+	 */
+	public int getX(){return x;}
+
+	/**
+	 * getter de la position y
+	 * @return y
+	 */
+	public int getY(){return y;}
+	
+	
+	/**
+	 * Ajout de la route passé en parametre a troncsSortants
+	 * @param route
+	 */
+	public void addTroncSortant(Route route){
+		this.troncSortants.add(route);
+	}
 }
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,8 +1,19 @@
 package test;
-import modele.*;
+import controleur.Controleur;
 
 public class Test {
 	public static void main(String[] args){
+		System.out.println("--------- Test ---------");
+		System.out.println("Test parsage plan.xml");
+		Controleur contJav = new Controleur();
+		String path = "C:/Users/bastien/Desktop/DevOO/xml/plan10x10.xml";
+		if(contJav.chargerPlan(path)){
+			System.out.println("Génération ok");
+			System.out.println(contJav.getGrapheRoutier().toString());
+		}else{
+			System.out.println("caca");
+		}
+		
 		
 	}
 
