7fb831501e2cbba3d821c5be79aa1c7533327667
2081 <robsnicolet@hotmail.fr>
2014-12-03 23:15:39 +0100

    debug

--- src/appcom/ServeurBuilder.java
+++ src/appcom/ServeurBuilder.java
@@ -130,8 +130,10 @@ public class ServeurBuilder {
 		
 		new ServiceControleur(c,"supprimer-livraison",this.serveur){
 			protected Reponse getReponse(String in){
+				System.err.println("Supp : "+in);
 				int idLivraison = Integer.parseInt(in);
 				this.getControleur().supprimerLivraison(idLivraison);
+				System.err.println("Supp ok");
 				return Reponse.succes("Suppression terminée.");
 			}
 		};
