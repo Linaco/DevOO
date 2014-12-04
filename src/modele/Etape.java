ae97fb0d4f519c653112d3e171bd327efa4173f5
LilCartell <lilian.cartellier01@orange.fr>
2014-12-04 04:53:48 +0100

    Plus d'infos dans étape, sauf que cette fois ça marche

--- src/modele/Etape.java
+++ src/modele/Etape.java
@@ -97,11 +97,17 @@ public class Etape {
 		String res = "";
 		String minutes = String.format("%02d", this.heureDePassage.getMinutes());
 		String heure = heureDePassage.getHours() + "h" + minutes ;
+		int idClient=-1;
+
 		
 		res += "<etape heurePassage=\""+heure+"\" secondesAttente=\""+ 
 				this.secondesAttenteAvantPassage +"\" idIntersection=\"" +
 				adresse.getId() + "\" idPlageHoraire=\"" + 
-				getPlage(plages) + "\" idClient=\""+livraison.getIdClient() +"\" adresse=\""+adresse.getId() +"\" />";
+				getPlage(plages) + "\">";
+		if(livraison != null){
+			res+="<livraison idClient=\""+idClient +"\" adresse=\""+adresse.getId() +"\" />";
+		}		
+		res+= "</etape>";
 		return res;
 	}
 	
