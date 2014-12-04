ee6d2548dab63788f4d0cbf406f534ea8d0dcff1
2081 <robsnicolet@hotmail.fr>
2014-12-05 00:20:52 +0100

    Avec la doc à jour tant qu'à faire...

--- js/devoo/out/api.js
+++ js/devoo/out/api.js
@@ -1,23 +1,33 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "Com",
         "Controleur",
-        "Vue"
+        "Vue",
+        "VueIntersection",
+        "VueLegende",
+        "VueRoute"
     ],
     "modules": [
         "appcom",
-        "controleur"
+        "controleur",
+        "vue"
     ],
     "allModules": [
         {
             "displayName": "appcom",
             "name": "appcom",
-            "description": "Controleur de l'application. Reçois les évènements utilisateurs de la vue, puis invoque\nles services adaptés de l'application java pour modifier les données."
+            "description": "Le package appcom permet la communication entre les différents morceaux de l'application \n(java/js)."
         },
         {
             "displayName": "controleur",
             "name": "controleur",
             "description": "Controleur de l'application. Reçois les évènements utilisateurs de la vue, puis invoque\nles services adaptés de l'application java pour modifier les données."
+        },
+        {
+            "displayName": "vue",
+            "name": "vue",
+            "description": "Vue de l'application, gère toutes les informations graphiques renvoyées à l'utilsateur."
         }
     ]
 } };
