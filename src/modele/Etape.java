17a18ac5b9fa76f3f173c3f57b1ce882c7f59a11
LilCartell <lilian.cartellier01@orange.fr>
2014-12-04 01:03:17 +0100

    Plus de Javadoc

--- src/modele/Etape.java
+++ src/modele/Etape.java
@@ -15,12 +15,11 @@ public class Etape {
 	private Intersection adresse;
 	private boolean aLivraison;
 
-    /**
-     * 
-     */
-    public Etape() {
-    }
-    
+	/**
+	 * Constructeur d'Etape    
+	 * @param heurePassage : l'heure de passage qui doit être associée à l'étape
+	 * @param adresse : l'intersection où se trouve l'étape
+	 */
     public Etape(Date heurePassage, Intersection adresse){
     	this.adresse = adresse;
     	this.heureDePassage = heurePassage;
@@ -29,40 +28,58 @@ public class Etape {
         aLivraison = false;
     }
 
-    /**
-     * 
-     */
     
 
     /**
-     * @return
+     * Getter sur l'heure de passage prévue à l'étape
+     * @return Date : l'heure à laquelle le passage à l'étape est prévu
      */
     public Date getHeurePassagePrevue() {
         // TODO implement here
         return this.heureDePassage;
     }
 
-    public Intersection getAdresse(){return this.adresse;}
     /**
-     * @param double
+     * Getter sur l'adresse de la livraison
+     * @return Intersection : l'intersection à laquelle se trouve l'étape
      */
+    public Intersection getAdresse(){return this.adresse;}
+    
+	/**
+	 * Setter de l'heure de passage prévue à l'étape
+	 * @param nouvelleHeure : la nouvelle heure de passage prévue à l'étape
+	 */
     public void setHeurePassagePrevue(Date nouvelleHeure) {
     	this.heureDePassage = nouvelleHeure;
-        // TODO implement here
     }
     
+    /**
+     * Permet d'indiquer que l'étape est associée à une livraison
+     */
     public void setaLivraison(){
     	this.aLivraison = true;
     }
     
+    /**
+     * Getter sur le booléen indiquant si l'étape est associée à une intersection
+     * @return boolean : true si et seulement si l'étape est associée à une intersection
+     */
     public boolean getaLivraison(){
     	return this.aLivraison;
     }
 
+    /**
+     * Getter sur le temps à attendre avant de pouvoir réaliser l'étape
+     * @return int : le temps d'attente avant de réaliser l'étape
+     */
     public int getAttenteAvantPassage() {
         return secondesAttenteAvantPassage;
     }
 
+    /**
+     * Setter du temps d'attente avant de pouvoir réaliser l'étape
+     * @param attenteAvantPassage : nouveau temps à attendre avant de passer à l'étape
+     */
     public void setAttenteAvantPassage(int attenteAvantPassage) {
         this.secondesAttenteAvantPassage = attenteAvantPassage;
     }
@@ -73,8 +90,8 @@ public class Etape {
     }
     
     /**
-     * Pas complet car il manque le lien avec la livraison
-     * @return String
+     * Permet d'obtenir une description XML de l'étape
+     * @return String : une chaîne de caractères contenant la description XML de l'étape
      */
 	public String toStringXML(List<PlageHoraire> plages){
 		String res = "";
@@ -88,6 +105,11 @@ public class Etape {
 		return res;
 	}
 	
+	/**
+	 * Permet d'obtenir l'identifiant de la plage horaire à laquelle l'étape est associée
+	 * @param plages : la liste des plages parmi lesquels l'étape est recherchée
+	 * @return int : l'identifiant de la plage horaire associé à l'étape, -1 si il n'y en a aucune
+	 */
 	public int getPlage(List<PlageHoraire> plages) {
 		for (PlageHoraire p: plages) {
                     if((heureDePassage.equals(p.getHeureDebut())||heureDePassage.after(p.getHeureDebut()))
